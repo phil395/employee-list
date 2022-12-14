2ccc013f7f6abd957d1d7b7f1ab1bc5796339709
phil395 <philiplgvn@gmail.com>
2022-12-14 11:45:07 +0300

    add interfaces: IEmployee, IEmployeeActions

---
+++ src/interfaces/IEmpolyeeActions.ts
@@ -0,0 +1,10 @@
+import { IEmployee } from "./IEmployee";
+
+type ToggleableAchievement = Extract<keyof IEmployee, 'bonus' | 'award'>;
+
+export interface IEmployeeActions {
+	toggleAchievement: (id: string, achievement: ToggleableAchievement) => void;
+	setSalary: (id: string, salary: number) => void;
+	deleteEmployee: (id: string) => void;
+	addEmployee: (name: string, salary: number) => void;
+}
