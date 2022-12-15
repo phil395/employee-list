0266dd4c771e121d37e05858fd8acc4c86edabcc
phil395 <philiplgvn@gmail.com>
2022-12-15 14:58:32 +0300

    Refactoring App component: add logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,53 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import { useMemo, useReducer, useState } from 'react';
+import { HeaderCta } from './components/header-cta/HeaderCta';
+import { HeaderInfo } from './components/header-info/HeaderInfo';
+import { List } from './components/list/List';
+import { initialEmployees } from './initialData/initialEmployees';
+import { IEmployee, ToggleableAchievement } from './interfaces/IEmployee';
+import { FilterValue } from './interfaces/IHeaderCta';
+import { IHeaderStats } from './interfaces/IHeaderStats';
+import { initReducer } from './reducer/init';
+import { reducer } from './reducer/reducer';
+import { curry } from './utils/curry';
+
+const MIN_SALARY_FILTER_VALUE = 1_000;
 
 export const App = () => {
+  // todo: переписать тип state в reducer - сделать его IEmployee[]
+  const [employees, dispatch] = useReducer(reducer, initialEmployees, initReducer);
+  const [searchValue, setSearchValue] = useState('');
+  const [filterValue, setFilterValue] = useState<FilterValue>('all');
+
+  const qty = useMemo<IHeaderStats>(() => ({
+    qtyEmployees: employees.employees.length,
+    qtyEmployeesWithBonus: employees.employees.filter(employee => employee.bonus).length
+  }), [employees]);
+
+  const listForRender = useMemo<IEmployee[]>(() => {
+    const filterBySearchValue = (employees: IEmployee[]) => {
+      return employees.filter(({ name }) => name.toLowerCase().includes(searchValue));
+    };
+    const filterByAchievement = (achievement: ToggleableAchievement) => {
+      return employees.employees.filter(employee => employee[achievement]);
+    };
+    const filterBySalary = (salary: number) => {
+      return employees.employees.filter(employee => employee.salary > salary);
+    };
+    switch (filterValue) {
+      case 'award': return curry(filterBySearchValue)(filterByAchievement)('award');
+      // case 'bonus': return curry(filterBySearchValue)(filterByAchievement)('bonus');  // button not implemented yet
+      case 'salary': return curry(filterBySearchValue)(filterBySalary)(MIN_SALARY_FILTER_VALUE);
+      default:
+      case 'all': return filterBySearchValue(employees.employees);
+    }
+  }, []);
+
 
   return (
     <div className="app">
+      <HeaderInfo {...qty} />
+      <HeaderCta setFilterValue={setFilterValue} setSearchValue={setSearchValue} />
+      {/* <List /> */}
 
     </div>
   );
